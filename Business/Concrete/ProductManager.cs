6b2c980028721da709b119f3ced09bca164ee517
kardem11 <simgdem6@gmail.com>
2022-07-30 21:50:40 +0300

    14/updated

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -29,7 +29,7 @@ namespace Business.Concrete
             _categoryService = categoryService;
         }
 
-
+        //[SecuredOperation("product.add, admin")]
         [ValidationAspect(typeof(ProductValidator))]
         public IResult Add(Product product)
         {
